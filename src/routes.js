8f370c020e4908e6136dd0f90068f9fd6c8c1d02
Zianatul Muna <zianatulmn@gmail.com>
2022-04-19 00:56:42 +0800

    FixGetAllBooks

---
+++ src/routes.js
@@ -0,0 +1,20 @@
+const {
+  addBookHandler,
+  getAllBooksHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+
+];
+
+module.exports = routes;
